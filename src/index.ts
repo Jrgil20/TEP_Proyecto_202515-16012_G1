0515e70a0bf7aa6c0c7da4dd9ddf2e57a350de8f
JRGIL12 <108557695+JRGIL12@users.noreply.github.com>
2024-12-24 16:43:39 -0400

    Add MongoDB connection and Docker configuration for the project

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,12 @@ import express from 'express';
 
 const app = express();
 const port = 3000;
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://127.0.0.1:27017/TEP_project')
+  .then(() => console.log('Conectado a MongoDB'))
+  .catch((err: unknown) => console.error(err));
+
 
 app.get('/', (req, res) => {
   res.send('Hello, world!');
