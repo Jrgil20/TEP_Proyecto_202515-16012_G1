7a8f1fb3706bf357c7bae2ebc90f88ffbc59799c
Massiel <164444049+massiperozo@users.noreply.github.com>
2024-12-31 16:37:26 +0000

    Se escribio el codigo minimo necesario para que la prueba pase

--- src/index.ts
+++ src/index.ts
@@ -3,23 +3,11 @@ import mongoose from 'mongoose';
 import Joke from './models/joke';
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
 
-// Función para conectar a MongoDB
-const connectDB = async (uri: string) => {
-  try {
-    await mongoose.connect(uri);
-    console.log('Conectado a MongoDB');
-  } catch (err) {
-    console.error('Error al conectar a MongoDB:', err);
-    process.exit(1);
-  }
-};
-
-// Conectar a MongoDB solo si no estamos en modo de prueba
-if (process.env.NODE_ENV !== 'test') {
-  connectDB('mongodb://127.0.0.1:27017/TEP_project');
-}
+//mongoose.connect('mongodb://127.0.0.1:27017/TEP_project')
+//  .then(() => console.log('Conectado a MongoDB'))
+//  .catch((err: unknown) => console.error('Error al conectar a MongoDB:', err));
 
 app.use(express.json());
 
@@ -46,6 +34,7 @@ app.put('/api/jokes/:id', async (req, res) => {
   try {
     const { id } = req.params;
     
+    // Verificar si el ID es válido
     if (!mongoose.Types.ObjectId.isValid(id)) {
       return res.status(404).json({ message: "Chiste no encontrado" });
     }
@@ -68,6 +57,26 @@ app.put('/api/jokes/:id', async (req, res) => {
   }
 });
 
+app.delete('/api/jokes/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(404).json({ message: "Chiste no encontrado" });
+    }
+
+    const joke = await Joke.findByIdAndDelete(id);
+    if (!joke) {
+      return res.status(404).json({ message: "Chiste no encontrado" });
+    }
+    
+    res.json({ message: "Chiste eliminado" });
+  } catch (error) {
+    console.error('Error al eliminar el chiste:', error);
+    res.status(500).json({ message: "Error al eliminar el chiste" });
+  }
+});
+
 if (process.env.NODE_ENV !== 'test') {
   app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
@@ -75,3 +84,4 @@ if (process.env.NODE_ENV !== 'test') {
 }
 
 export default app;
+
